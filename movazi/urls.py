b2390858cadf385ee093a522b680ae705c4db133
Ahmad Sharifian <ahmad.sharif.abc@gmail.com>
2019-04-27 20:32:06 +0430

    up site 2

--- movazi/urls.py
+++ movazi/urls.py
@@ -46,4 +46,4 @@ urlpatterns += static(settings.MEDIA_URL,
 document_root = settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL,
-document_root = settings.STATICFILES_ROOT)
+document_root = settings.STATIC_ROOT)
