4a549334d811a162a3deb74ac3d7d3f050bdc9bb
Ahmad Sharifian <ahmad.sharif.abc@gmail.com>
2019-04-27 21:23:45 +0430

    up site 6

--- movazi/settings.py
+++ movazi/settings.py
@@ -95,7 +95,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
+    'django.contrib.sessions',
     # 'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
