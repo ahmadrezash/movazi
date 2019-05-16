dbf0a713767988e98332e77914c6d381adccec4c
Ahmad Sharifian <ahmad.sharif.abc@gmail.com>
2019-05-16 09:50:56 +0430

    bug fix (lag-tile-paralex)

--- movazi/settings.py
+++ movazi/settings.py
@@ -167,7 +167,7 @@ WSGI_APPLICATION = 'movazi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'media/db.sqlite3'),
     }
 }
 # DATABASES = {
