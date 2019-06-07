fee0f5aac8b0bdff622c56c9a5ba9f1d9c1aad69
ahmad <ahmad.sharif.abc@gmail.com>
2019-06-08 02:13:40 +0430

    Start again after ramadan

--- movazi/settings.py
+++ movazi/settings.py
@@ -108,7 +108,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    # 'django.contrib.messages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
     'panel',
