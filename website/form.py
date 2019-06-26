45c77485e589b009fac888110845079ae1e87801
ahmad <ahmad.sharif.abc@gmail.com>
2019-06-26 19:26:31 +0430

    conatact-us p2

--- website/form.py
+++ website/form.py
@@ -5,6 +5,7 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label='نام')
     email = forms.EmailField(label='ایمیل')
     message = forms.CharField(widget=forms.Textarea, label='پیام')
+    phone_number = forms.CharField(max_length=100, label='شماره تلفن')
     # labels = {
     #     "name": "نام    "
     # }
