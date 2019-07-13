60a9f73de139dcd29fd39bdd813ce7d00d30be90
ahmad.sharif.abc <ahmad.sharif.abc@gmail.com>
2019-07-13 16:36:08 +0430

    conatact-us p2

--- movazi/suser.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import get_user_model
-
-User = get_user_model()  # get the currently active user model,
-
-User.objects.filter(username='admin').exists() or \
-    User.objects.create_superuser('ahmad', 'ahmad@example.com', '1')
